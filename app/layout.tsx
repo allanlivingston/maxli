c99d69bea634d15cb4aece208ba30f8603594460
bsus <75512628+allanlivingston@users.noreply.github.com>
2024-11-02 17:12:18 -0700

    changed page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "OnPoint Batteries",
+  title: "Maximum Lithium",
   description: "Eco-friendly power solutions for your adventures",
   icons: {
     icon: [
